2ca87a558d39bbb4a439032060c8d614c8133792
Francisco Guimarães <francisco.cpg@gmail.com>
2017-08-15 15:29:18 -0300

    Adding FUNCTION_NAME as parameter

--- scripts/deploy-lambda.sh
+++ scripts/deploy-lambda.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm lambda.zip
+FUNCTION_NAME=${1:-lambci-build}
+rm -f lambda.zip
 set -e
 zip -X -r lambda.zip *
 aws s3 cp lambda.zip s3://franciscocpg-lambda
-aws lambda update-function-code --function-name lambci-build --s3-bucket franciscocpg-lambda --s3-key lambda.zip
+aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket franciscocpg-lambda --s3-key lambda.zip
