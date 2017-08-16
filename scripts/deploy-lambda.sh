db374f980612d233a283ab138995360377d5aa2f
Francisco Guimarães <francisco.cpg@gmail.com>
2017-08-16 20:11:41 -0300

    Adding bucket env to deploy-lambda

--- scripts/deploy-lambda.sh
+++ scripts/deploy-lambda.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 FUNCTION_NAME=${1:-lambci-build}
+S3_BUCKET=${S3_BUCKET:-franciscocpg-lambda}
 rm -f lambda.zip
 set -e
 zip -X -r lambda.zip *
-aws s3 cp lambda.zip s3://franciscocpg-lambda
-aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket franciscocpg-lambda --s3-key lambda.zip
+aws s3 cp lambda.zip s3://$S3_BUCKET
+aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket $S3_BUCKET --s3-key lambda.zip
